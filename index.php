755292a53b0e457479f2854422fccf7f001232aa
acj <gitlab@acjs.net>
2019-08-01 23:26:45 +0200

    Freshen up.

--- index.php
+++ index.php
@@ -1,64 +1,68 @@
 <!DOCTYPE html>
-<html dir=ltr itemscope itemtype=http://schema.org/VisualArtwork lang=en>
+<html dir="ltr" itemscope itemtype="http://schema.org/VisualArtwork" lang="en">
 <head>
-<meta charset=utf-8>
-<meta itemprop=artform content=Digital>
-<meta name=apple-mobile-web-app-capable content=yes>
-<meta name=apple-mobile-web-app-status-bar-style content=black-translucent>
-<meta name=description property="og:description" content="Website + domain name, 2016.">
-<meta name=robots content="follow, index, noarchive">
-<meta name=viewport content="initial-scale=1.0,shrink-to-fit=no,width=device-width">
-<meta property="fb:admins" content="509248955">
-<meta property="og:image" content="https://hyperhyper.link/images/opengraph.jpg">
-<meta property="og:image:height" content="288">
-<meta property="og:image:width" content="512">
-<meta property="og:title" content="Hyper Hyper Link">
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://hyperhyper.link/">
-<title itemprop=name>Hyper Hyper Link</title>
-<link itemprop=creator href=https://alexanderchristiaanjacob.com/>
-<link rel=author href=https://alexanderchristiaanjacob.com/>
-<link rel=canonical href=https://hyperhyper.link/>
-<script src="https://fast.fonts.net/jsapi/4e8d6e2b-570c-45cf-aa54-e6c1abe10eea.js"></script>
-<style>
+    <meta charset="utf-8">
+    <meta itemprop="artform" content="Digital">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="description" content="Website + domain name, 2016.">
+    <meta name="theme-color" content="#cccccc">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@ACJ">
+    <meta name="twitter:creator" content="@ACJ">
+    <meta name="viewport" content="initial-scale=1.0,shrink-to-fit=no,width=device-width">
+    <meta property="fb:admins" content="509248955">
+    <meta property="og:description" content="Website + domain name, 2016.">
+    <meta property="og:image" content="https://hyperhyper.link/images/opengraph.jpg">
+    <meta property="og:image:height" content="288">
+    <meta property="og:image:width" content="512">
+    <meta property="og:title" content="Hyper Hyper Link">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://hyperhyper.link/">
+    <title itemprop="name">Hyper Hyper Link</title>
+    <link itemprop="creator" href="https://alexanderchristiaanjacob.com/">
+    <link rel="author" href="https://alexanderchristiaanjacob.com/" title="Alexander Christiaan Jacob">
+    <link rel="canonical" href="https://hyperhyper.link/">
+    <link rel="help" href="https://github.com/acjbizar/hyperhyper.link">
+    <script src="https://fast.fonts.net/jsapi/4e8d6e2b-570c-45cf-aa54-e6c1abe10eea.js"></script>
+    <style>
 
-*{border:0;color:inherit;font-size:1em;font-weight:normal;list-style:none;margin:0;outline:0;padding:0;text-decoration:none}
+        *{border:0;color:inherit;font-size:1em;font-weight:normal;list-style:none;margin:0;outline:0;padding:0;text-decoration:none}
 
-a{text-shadow:0 0 12px rgba(255,255,255,.25)}
-body,html{height:100%}
-body{align-items:center;display:flex;justify-content:center}
-h1{font-size:calc(15vw + 15vh + 9vmin);line-height:.75;text-align:center;text-transform:uppercase}
-html{font:normal 100%/1.5 'DINNextW01-CondensedMed',sans-serif,serif;overflow:hidden}
+        a{text-shadow:0 0 12px rgba(255,255,255,.25)}
+        body,html{height:100%}
+        body{align-items:center;display:flex;justify-content:center}
+        h1{font-size:calc(15vw + 15vh + 9vmin);line-height:.75;text-align:center;text-transform:uppercase}
+        html{font:normal 100%/1.5 'DINNextW01-CondensedMed',sans-serif,serif;overflow:hidden}
 
-</style>
+    </style>
 </head>
-<body id=top>
-<audio data-source=https://www.freesound.org/people/kwahmah_02/sounds/256116/ id=click preload=auto src=sounds/click.mp3></audio>
-<h1><a href="#" id=link>Click here!</a></h1>
+<body id="top">
+<audio data-source="https://www.freesound.org/people/kwahmah_02/sounds/256116/" id="click" preload="auto" src="sounds/click.mp3"></audio>
+<h1><a href="#" id="link">Click here!</a></h1>
 <script>
+    "use strict";
 
-var speed = 92;
+    var speed = 92;
+    var intervalID = window.setInterval(run, speed);
+    var clickingSound = document.getElementById('click');
+    var bg = document.getElementById("top");
 
-var intervalID = window.setInterval(run, speed);
+    function run() {
+        clickingSound.pause();
+        clickingSound.currentTime = 0;
+        clickingSound.play();
+        bg.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        bg.style.color = bg.style.color === 'blue' ? 'purple' : 'blue';
+    }
 
-clickingSound = document.getElementById('click');
-bg = document.getElementById("top");
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-function run() {
-  clickingSound.pause();
-  clickingSound.currentTime = 0;
-  clickingSound.play();
-  bg.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-  bg.style.color = bg.style.color === 'blue' ? 'purple' : 'blue';
-}
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-6227584-61', 'auto');
-  ga('send', 'pageview');
+    ga('create', 'UA-6227584-61', 'auto');
+    ga('send', 'pageview');
 
 </script>
 </body>
